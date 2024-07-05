f3f03b786a2c09064a8e707087fb458afa35a2c7
Eddie Carraro <eddie.carraro@studenti.unipd.it>
2024-07-05 08:45:03 +0200

    added files for performance measurement

---
+++ include/performance_measurement.h
@@ -0,0 +1,18 @@
+//Author: Eddie Carraro
+
+#ifndef PERFORMANCE_MEASUREMENT
+#define PERFORMANCE_MEASUREMENT
+
+class performance_measurement
+{
+    public:
+    float balls_detection_performance(int x, int y, int width, int height, int ball_ID);
+    float balls_segmentation_performance();
+    float table_segmentation_performance();
+
+    private:
+
+};
+
+
+#endif
---
+++ src/performance_measurement.cpp
@@ -0,0 +1,19 @@
+//Author: Eddie Carraro
+
+#include "performance_measurement.h"
+
+
+float performance_measurement::balls_detection_performance(int x, int y, int width, int height, int ball_ID)
+{
+    return 0.0f;
+}
+
+float performance_measurement::balls_segmentation_performance()
+{
+    return 0.0f;
+}
+
+float performance_measurement::table_segmentation_performance()
+{
+    return 0.0f;
+}
