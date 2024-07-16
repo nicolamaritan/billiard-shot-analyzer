f0854cc9ec98d222cd3154f1c91de2cca1eb3d3e
Nicola Maritan <nicolamaritan01@gmail.com>
2024-07-17 00:09:51 +0200

    implemented all images evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -12,24 +12,26 @@ int main()
 
     vector<Mat> found_table_masks;
     vector<Mat> ground_truth_table_masks;
+    vector<String> frames_filenames;
 
     glob("*.png", filenames, true);
     for (auto filename : filenames)
     {
-        if (filename.find("masks") == String::npos && filename.find("first") != String::npos)
+        if (filename.find("masks") == String::npos && (filename.find("first") != String::npos || filename.find("last") != String::npos))
         {
             Mat img = imread(filename);
             Mat img_segmentation;
 
             frame_segmentation(img, img_segmentation);
             found_table_masks.push_back(img_segmentation);
+            frames_filenames.push_back(filename);
         }
     }
 
     //performance computation
     for (auto filename : filenames)
     {
-        if (filename.find("masks") != String::npos && filename.find("first") != String::npos)
+        if (filename.find("masks") != String::npos && (filename.find("first") != String::npos || filename.find("last") != String::npos))
         {
             Mat img = imread(filename, CV_8UC1);
             ground_truth_table_masks.push_back(img);
@@ -37,13 +39,11 @@ int main()
     }
 
     cout << "import and segmentation done" << endl;
-    for (int i = 0; i < found_table_masks.size() - 1; i++)
+    for (int i = 0; i < found_table_masks.size(); i++)
     {
-        imshow("ground truth", ground_truth_table_masks[i]);
-        imshow("found", found_table_masks[i]);
+        cout << frames_filenames.at(i) << endl;
         performance_measurement measures;
         measures.evaluate_playing_field_segmentation(ground_truth_table_masks[i], found_table_masks[i]);
-        waitKey(0);
     }
 
 
--- src/performance_measurement.cpp
+++ src/performance_measurement.cpp
@@ -15,17 +15,30 @@ float performance_measurement::evaluate_class(const cv::Mat &found_mask, const c
     Mat union_class_mask, intersection_class_mask;
     bitwise_or(found_class_mask, ground_truth_class_mask, union_class_mask);
     bitwise_and(found_class_mask, ground_truth_class_mask, intersection_class_mask);
-    return static_cast<float>(countNonZero(intersection_class_mask)) / static_cast<float>(countNonZero(union_class_mask));
+
+    float union_area = static_cast<float>(countNonZero(union_class_mask));
+    if (union_area == 0)
+        return 1;
+    float intersection_area = static_cast<float>(countNonZero(intersection_class_mask));
+    return  intersection_area / union_area;
 }
 
 float performance_measurement::evaluate_playing_field_segmentation(const cv::Mat &found_mask, const cv::Mat &ground_truth_mask)
 {
-    cout << "background iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::background) << endl;
-    cout << "cue iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::cue) << endl;
-    cout << "black iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::black) << endl;
-    cout << "solids iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::solids) << endl;
-    cout << "stripes iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::stripes) << endl;
-    cout << "playing iou: " << evaluate_class(found_mask, ground_truth_mask, segmentation_label::playing_field) << endl;
+    float iou_background = evaluate_class(found_mask, ground_truth_mask, segmentation_label::background);
+    float iou_cue = evaluate_class(found_mask, ground_truth_mask, segmentation_label::cue);
+    float iou_black = evaluate_class(found_mask, ground_truth_mask, segmentation_label::black);
+    float iou_solids = evaluate_class(found_mask, ground_truth_mask, segmentation_label::solids);
+    float iou_stripes = evaluate_class(found_mask, ground_truth_mask, segmentation_label::stripes);
+    float iou_playing_field = evaluate_class(found_mask, ground_truth_mask, segmentation_label::playing_field);
+
+    cout << "background iou: " << iou_background << endl;
+    cout << "cue iou: " << iou_cue << endl;
+    cout << "black iou: " << iou_black << endl;
+    cout << "solids iou: " << iou_solids << endl;
+    cout << "stripes iou: " << iou_stripes << endl;
+    cout << "playing iou: " << iou_playing_field << endl;
+    cout << "mean iou: " << (iou_background + iou_cue + iou_black + iou_solids + iou_stripes + iou_playing_field) / 6 << endl; 
     cout << "--------------------------" << endl;
 
     return 0;
