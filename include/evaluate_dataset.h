0d8b27fd34905507c11528cb12ffc7955b474d82
Eddie <eddie.carraro@studenti.unipd.it>
2024-07-17 19:04:35 +0200

    Added documentation to evaluate function

--- include/evaluate_dataset.h
+++ include/evaluate_dataset.h
@@ -4,6 +4,12 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+
+/**
+ * @brief Processes images and associated data from a specified directory, evaluates the performance of table segmentation and ball localization, and writes the results (the mean Intersection over Union and the mean Average Precision) to a text file.
+ * 
+ * @param path A string representing the directory path containing the images and ground truth files.
+ */
 void evaluate(std::string path);
 
 
