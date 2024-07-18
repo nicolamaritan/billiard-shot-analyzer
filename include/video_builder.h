90a5b01cd472695a430352b667ffdf581d300fed
Nicola Maritan <nicolamaritan01@gmail.com>
2024-07-18 17:14:08 +0200

    added doc

--- include/video_builder.h
+++ include/video_builder.h
@@ -1,3 +1,4 @@
+// Author: Nicola Maritan
 #ifndef VIDEO_BUILDER_H
 #define VIDEO_BUILDER_H
 
@@ -7,17 +8,72 @@
 class video_builder
 {
 public:
-    void build_videos(std::string dataset_path);
+    /**
+     * @brief Processes a vector of input video files of games and produces a set of output video with the minimap,
+     * saved as files.
+     *
+     * @param filename The name of the input video file.
+     */
+    void build_videos(const std::string &dataset_path);
+
+    /**
+     * @brief Processes an input video file of a game and produces an output video with the minimap, saved as file.
+     *
+     * @param filename The name of the input video file.
+     */
+    void build_video(const std::string &filename);
 
 private:
-    void build_output_frames(std::string video_filename, std::vector<cv::Mat> &output_frames);
+    /**
+     * @brief Builds output frames from an input video file.
+     *
+     * @param filename The name of the input video file.
+     * @param output_frames A vector to store the output frames.
+     */
+    void build_output_frames(const std::string &video_filename, std::vector<cv::Mat> &output_frames);
+
+    /**
+     * @brief Combines a video frame and a minimap into a single output frame.
+     *
+     * @param frame The original video frame.
+     * @param minimap The minimap to be included in the output frame.
+     * @param dst The destination Mat object to store the combined output frame.
+     */
     void build_output_frame(const cv::Mat &frame, const cv::Mat &minimap, cv::Mat &dst);
-    void build_video(std::vector<cv::Mat> output_frames, std::string output_filename);
+
+    /**
+     * @brief Constructs a video file from a sequence of output frames.
+     *
+     * @param output_frames Vector of frames to be written to the video.
+     * @param output_filename Name of the output video file.
+     */
+    void build_video_from_output_frames(const std::vector<cv::Mat> &output_frames, const std::string &output_filename);
+
+    /**
+     * @brief Clears the input video information.
+     */
     void clear_input_video_info();
 
+    /**
+     * @brief Rescales a bounding box by a given scale factor and ensures it does not exceed a maximum size.
+     *
+     * @param bbox The original bounding box to be rescaled.
+     * @param scale The scaling factor to be applied to the bounding box.
+     * @param max_size The maximum allowed size for the bounding box.
+     * @return A new Rect object representing the rescaled bounding box.
+     */
+    cv::Rect rescale_bounding_box(const cv::Rect &bbox, float scale, int max_size);
+
+    // Vector to store output frames
     std::vector<cv::Mat> output_frames;
+
+    // Frame rate of the input video
     double input_video_fps;
+
+    // Size of the input video frames
     cv::Size input_video_size;
+
+    // Codec used for the input video
     int input_video_codec;
 };
 
--- src/video_builder.cpp
+++ src/video_builder.cpp
@@ -1,3 +1,4 @@
+// Author: Nicola Maritan
 #include "video_builder.h"
 #include "minimap.h"
 #include "playing_field_localization.h"
@@ -17,7 +18,7 @@ using namespace cv;
 using namespace std;
 namespace fs = std::filesystem;
 
-void video_builder::build_video(vector<Mat> output_frames, string output_filename)
+void video_builder::build_video_from_output_frames(const vector<Mat> &output_frames, const string &output_filename)
 {
     VideoWriter output_video;
     output_video.open(output_filename, input_video_codec, input_video_fps, input_video_size, true);
@@ -32,7 +33,27 @@ void video_builder::build_video(vector<Mat> output_frames, string output_filenam
     }
 }
 
-void video_builder::build_videos(string dataset_path)
+void video_builder::build_video(const string &filename)
+{
+    vector<String> filenames;
+
+    fs::path output_directory("output");
+    fs::path videos_directory("videos");
+    output_directory /= videos_directory;
+    fs::create_directories(output_directory);
+
+    clear_input_video_info();
+    build_output_frames(filename, output_frames);
+
+    fs::path output_path;
+    output_path /= output_directory;
+    output_path /= fs::path(filename).filename();
+
+    cout << "Creating " << output_path.string() << "." << endl;
+    build_video_from_output_frames(output_frames, output_path.string());
+}
+
+void video_builder::build_videos(const string &dataset_path)
 {
     vector<String> filenames;
     glob(dataset_path + "*.mp4", filenames, true);
@@ -53,11 +74,28 @@ void video_builder::build_videos(string dataset_path)
         output_path /= fs::path(filename).filename();
 
         cout << "Creating " << output_path.string() << "." << endl;
-        build_video(output_frames, output_path.string());
+        build_video_from_output_frames(output_frames, output_path.string());
     }
 }
 
-void video_builder::build_output_frames(string filename, vector<Mat> &output_frames)
+Rect video_builder::rescale_bounding_box(const Rect &bbox, float scale, int max_size)
+{
+    int new_width = static_cast<int>(bbox.width * scale);
+    int new_height = static_cast<int>(bbox.height * scale);
+
+    if (new_width > max_size)
+        new_width = max_size;
+    if (new_height > max_size)
+        new_height = max_size;
+
+    // Calculate the new top-left corner to maintain the center of the bounding box
+    int new_x = bbox.x + (bbox.width - new_width) / 2;
+    int new_y = bbox.y + (bbox.height - new_height) / 2;
+
+    return cv::Rect(new_x, new_y, new_width, new_height);
+}
+
+void video_builder::build_output_frames(const string &filename, vector<Mat> &output_frames)
 {
     VideoCapture input_video(filename);
     if (!input_video.isOpened())
@@ -66,7 +104,7 @@ void video_builder::build_output_frames(string filename, vector<Mat> &output_fra
     input_video_codec = static_cast<int>(input_video.get(CAP_PROP_FOURCC));
     input_video_fps = input_video.get(CAP_PROP_FPS);
     input_video_size = Size(static_cast<int>(input_video.get(CAP_PROP_FRAME_WIDTH)),
-                  static_cast<int>(input_video.get(CAP_PROP_FRAME_HEIGHT)));
+                            static_cast<int>(input_video.get(CAP_PROP_FRAME_HEIGHT)));
 
     Mat first_frame, frame;
     input_video.read(first_frame);
@@ -76,16 +114,28 @@ void video_builder::build_output_frames(string filename, vector<Mat> &output_fra
     balls_localizer balls_loc(pl_field_loc.get_localization());
     balls_loc.localize(first_frame);
 
-    Ptr<legacy::MultiTracker> multiTracker = legacy::MultiTracker::create();
+    Ptr<legacy::MultiTracker> multi_tracker = legacy::MultiTracker::create();
 
     // Initialize the trackers for each detected bounding box
-    for (const auto &roi : balls_loc.get_bounding_boxes())
+    for (const auto &bbox : balls_loc.get_bounding_boxes())
     {
-        multiTracker->add(legacy::TrackerCSRT::create(), first_frame, roi);
+        /*
+            The bounding boxes provided to the trackers are scaled by a factor
+            greater than 1. This approach is adopted because we have observed that
+            an enlarged bounding box enhances the tracker's ability to follow the
+            ball accurately. Retaining the original detected bounding boxes often
+            results in the tracker conflating adjacent or colliding balls. However,
+            it is crucial that the scaling factor is not excessively large, as
+            this would impair the tracker's ability to follow the ball effectively.
+        */
+        const float BOUNDING_BOX_RESCALE = 1.3;
+        const int MAX_BOUNDING_BOX_SIZE = 30;
+        multi_tracker->add(legacy::TrackerCSRT::create(), first_frame, rescale_bounding_box(bbox, BOUNDING_BOX_RESCALE, MAX_BOUNDING_BOX_SIZE));
     }
 
     // Load minimap
-    Mat pool_table_map = imread("pool_table.png");
+    const string MINIMAP_IMAGE_FILENAME = "pool_table.png";
+    Mat pool_table_map = imread(MINIMAP_IMAGE_FILENAME);
     Mat trajectories = pool_table_map.clone();
     minimap mini(pl_field_loc.get_localization(), balls_loc.get_localization());
 
@@ -94,24 +144,23 @@ void video_builder::build_output_frames(string filename, vector<Mat> &output_fra
     vector<int> stripes_indeces;
     int black_index;
     int cue_index;
-    mini.get_balls_pos(multiTracker->getObjects(), initial_balls_pos);
+    mini.get_balls_pos(multi_tracker->getObjects(), initial_balls_pos);
     mini.draw_initial_minimap(initial_balls_pos, balls_loc.get_localization(), solids_indeces, stripes_indeces, black_index, cue_index, first_frame, pool_table_map);
-    imshow("initial minimap", pool_table_map);
-    vector<Rect2d> old_balls_bounding_boxes = multiTracker->getObjects();
+    vector<Rect2d> old_balls_bounding_boxes = multi_tracker->getObjects();
 
     while (input_video.read(frame))
     {
 
-        multiTracker->update(frame);
+        multi_tracker->update(frame);
 
         vector<Point> old_balls_pos;
         mini.get_balls_pos(old_balls_bounding_boxes, old_balls_pos);
 
         vector<Point> current_balls_pos;
-        mini.get_balls_pos(multiTracker->getObjects(), current_balls_pos);
+        mini.get_balls_pos(multi_tracker->getObjects(), current_balls_pos);
 
         mini.draw_minimap(old_balls_pos, current_balls_pos, solids_indeces, stripes_indeces, black_index, cue_index, frame, trajectories, pool_table_map);
-        old_balls_bounding_boxes = multiTracker->getObjects();
+        old_balls_bounding_boxes = multi_tracker->getObjects();
 
         Mat output_frame;
         build_output_frame(frame, pool_table_map, output_frame);
