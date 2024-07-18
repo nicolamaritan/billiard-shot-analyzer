b9b4df74b6db99e4a9ec947c9156a312869328d1
Nicola Maritan <nicolamaritan01@gmail.com>
2024-07-18 14:20:30 +0200

    employed file load lib in generate segm and detect

--- src/generate_masks_and_detection.cpp
+++ src/generate_masks_and_detection.cpp
@@ -1,6 +1,7 @@
 #include "balls_localization.h"
 #include "frame_segmentation.h"
 #include "frame_detection.h"
+#include "file_loading.h"
 
 #include <fstream>
 #include <filesystem>
@@ -19,36 +20,35 @@ int main(int argc, char **argv)
     fs::create_directories(output_directory);   // ./output/masks_and_detection
 
     vector<String> filenames;
-    glob(dataset_path + "*.png", filenames, true);
+    get_frame_files(dataset_path, filenames);
     for (auto filename : filenames)
     {
-        if (filename.find("masks") == String::npos && (filename.find("first") != String::npos || filename.find("last") != String::npos))
-        {
-            Mat frame = imread(filename);
-            Mat frame_segmentation;
-            Mat frame_segmentation_background_preserved;
-            Mat frame_detection;
+        Mat frame = imread(filename);
+        Mat frame_segmentation;
+        Mat frame_segmentation_background_preserved;
+        Mat frame_detection;
 
-            fs::path file_path = fs::path(filename);
-            fs::path clip_game_directory;
-            clip_game_directory /= output_directory;
-            clip_game_directory /= file_path.parent_path().parent_path().filename(); // Create directory of type "game{a}_clip{b}, etc"  
-            fs::create_directory(clip_game_directory);
+        fs::path file_path = fs::path(filename);
+        fs::path clip_game_directory;
+        clip_game_directory /= output_directory;
+        clip_game_directory /= file_path.parent_path().parent_path().filename(); // Create directory of type "game{a}_clip{b}"  
+        fs::create_directory(clip_game_directory);
 
-            clip_game_directory /= fs::path(filename).filename();   // output/masks_and_detection/game{a}_clip{b}
+        clip_game_directory /= fs::path(filename).filename();   // {dataset}/output/masks_and_detection/game{a}_clip{b}
 
-            const string SEGMENTATION_FILENAME = "_segmentation"; 
-            const string SEGMENTATION_BACKGROUND_PRESERVED_FILENAME = "_segmentation_background_preserved"; 
-            const string DETECTION_FILENAME = "_detection"; 
+        const string SEGMENTATION_FILENAME = "_segmentation"; 
+        const string SEGMENTATION_BACKGROUND_PRESERVED_FILENAME = "_segmentation_background_preserved"; 
+        const string DETECTION_FILENAME = "_detection"; 
 
-            get_colored_frame_segmentation(frame, frame_segmentation, false);
-            get_colored_frame_segmentation(frame, frame_segmentation_background_preserved, true);
-            get_frame_detection(frame, frame_detection);
+        get_colored_frame_segmentation(frame, frame_segmentation, false);
+        get_colored_frame_segmentation(frame, frame_segmentation_background_preserved, true);
+        get_frame_detection(frame, frame_detection);
 
-            // Save files
-            imwrite(clip_game_directory.string() + SEGMENTATION_FILENAME, frame_segmentation);
-            imwrite(clip_game_directory.string() + SEGMENTATION_BACKGROUND_PRESERVED_FILENAME, frame_segmentation_background_preserved);
-            imwrite(clip_game_directory.string() + DETECTION_FILENAME, frame_detection);
-        }
+        // Save files
+        imwrite(clip_game_directory.string() + SEGMENTATION_FILENAME, frame_segmentation);
+        imwrite(clip_game_directory.string() + SEGMENTATION_BACKGROUND_PRESERVED_FILENAME, frame_segmentation_background_preserved);
+        imwrite(clip_game_directory.string() + DETECTION_FILENAME, frame_detection);
     }
+
+    return 0;
 }
