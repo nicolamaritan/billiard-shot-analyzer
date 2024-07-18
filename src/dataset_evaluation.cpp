a3110a2e8d656b71237f914c84168eb607e4624b
Nicola Maritan <nicolamaritan01@gmail.com>
2024-07-18 13:16:53 +0200

    polished code, added functions in file loading

--- src/dataset_evaluation.cpp
+++ src/dataset_evaluation.cpp
@@ -17,7 +17,6 @@ void evaluate(std::string dataset_path)
 {
     const string OUTPUT_DIRECTORY = "output";
     const string PERFORMANCE_FILE = "performance.txt";
-    const string PNG_EXTENSION = "*.png";
 
     fs::path output_directory(OUTPUT_DIRECTORY);
     fs::create_directory(output_directory);
@@ -29,45 +28,35 @@ void evaluate(std::string dataset_path)
     vector<String> frames_filenames;
     vector<balls_localization> predicted_balls_localizations;
     vector<balls_localization> ground_truth_balls_localizations;
-    glob(dataset_path + PNG_EXTENSION, filenames, true);
 
+    get_frame_files(dataset_path, filenames);
     for (auto filename : filenames)
     {
-        if (is_frame_file(filename))
-        {
-            Mat img = imread(filename);
-            Mat img_segmentation;
-            balls_localization localization;
+        Mat img = imread(filename);
+        Mat img_segmentation;
+        balls_localization localization;
 
-            get_frame_segmentation(img, img_segmentation);
-            get_balls_localization(img, localization);
+        get_frame_segmentation(img, img_segmentation);
+        get_balls_localization(img, localization);
 
-            predicted_table_masks.push_back(img_segmentation);
-            frames_filenames.push_back(filename);
-            predicted_balls_localizations.push_back(localization);
-        }
+        predicted_table_masks.push_back(img_segmentation);
+        frames_filenames.push_back(filename);
+        predicted_balls_localizations.push_back(localization);
     }
 
+    get_mask_files(dataset_path, filenames);
     for (auto filename : filenames)
     {
-        if (is_mask_frame_file(filename))
-        {
-            Mat img = imread(filename, CV_8UC1);
-            ground_truth_table_masks.push_back(img);
-        }
+        Mat img = imread(filename, CV_8UC1);
+        ground_truth_table_masks.push_back(img);
     }
 
-    // Bounding box load
-    vector<String> txt_filenames;
-    glob(dataset_path + "*.txt", txt_filenames, true);
-    for (auto txt_filename : txt_filenames)
+    get_bounding_boxes_files(dataset_path, filenames);
+    for (auto filename : filenames)
     {
-        if (is_bounding_box_file(txt_filename))
-        {
-            balls_localization ground_truth;
-            load_ground_truth_localization(txt_filename, ground_truth);
-            ground_truth_balls_localizations.push_back(ground_truth);
-        }
+        balls_localization ground_truth;
+        load_ground_truth_localization(filename, ground_truth);
+        ground_truth_balls_localizations.push_back(ground_truth);
     }
 
     for (int i = 0; i < predicted_table_masks.size(); i++)
