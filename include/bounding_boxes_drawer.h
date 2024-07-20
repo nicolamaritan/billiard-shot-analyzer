1321d118575047a273f322e5174b844cd44ce0f7
Nicola Maritan <nicolamaritan01@gmail.com>
2024-07-20 20:06:52 +0200

    bboxes videos

---
+++ include/bounding_boxes_drawer.h
@@ -0,0 +1,28 @@
+#ifndef BOUNDING_BOXES_DRAWER
+#define BOUNDING_BOXES_DRAWER
+
+#include "balls_localization.h"
+#include "playing_field_localization.h"
+
+/**
+ * @brief Classes that draws the bounding boxes around detected balls.
+ */
+class bounding_boxes_drawer
+{
+public:
+    bounding_boxes_drawer(const playing_field_localization &plf_localization, const balls_localization &blls_localization, const std::vector<cv::Rect2d> &tracker_bboxes);
+    void draw(const cv::Mat &frame, cv::Mat &dst, const std::vector<cv::Rect2d> &updated_balls_bboxes);
+
+private:
+    void draw_transparent_rect(cv::Mat &image, cv::Rect rect, cv::Scalar color, double alpha);
+
+    // Indeces of the objects tracked by the multitracker, they are constant during multitracker life time.
+    int cue_index;                    // Cue ball index.
+    int black_index;                  // Black ball index.
+    std::vector<int> solids_indeces;  // Solid balls indeces.
+    std::vector<int> stripes_indeces; // Stripe balls indeces.
+    playing_field_localization playing_field;
+    balls_localization balls;
+};
+
+#endif
--- src/video_builder.cpp
+++ src/video_builder.cpp
@@ -3,6 +3,7 @@
 #include "minimap.h"
 #include "playing_field_localization.h"
 #include "balls_localization.h"
+#include "bounding_boxes_drawer.h"
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
@@ -24,48 +25,31 @@ void video_builder::build_videos(const string &dataset_path)
     const string MP4_EXTENSION = "*.mp4";
     glob(dataset_path + MP4_EXTENSION, filenames, true);
 
-    fs::path output_directory("output");
-    fs::path videos_directory("videos");
     output_directory /= videos_directory;
-    fs::create_directories(output_directory);
+    fs::create_directories(output_directory / minimap_directory);
+    fs::create_directories(output_directory / last_frames_minimap_directory);
+    fs::create_directories(output_directory / bboxes_directory);
 
     for (String filename : filenames)
     {
-        fs::path output_path;
-        output_path /= output_directory;
-        output_path /= fs::path(filename).filename();
+        fs::path output_path = output_directory / minimap_directory / fs::path(filename).filename();
+        fs::path output_path_bboxes = output_directory / bboxes_directory / fs::path(filename).filename();
         cout << "Generating " << output_path.string() << "..." << endl;
+        cout << "Generating " << output_path_bboxes.string() << "..." << endl;
 
         clear_input_video_info();
 
-        build_output_frames(filename, output_frames);
+        build_output_frames(filename, output_frames, bboxes_output_frames);
 
         build_video_from_output_frames(output_frames, output_path.string());
+        build_video_from_output_frames(bboxes_output_frames, output_path_bboxes.string());
+
         cout << "Generated " << output_path.string() << "." << endl;
+        cout << "Generated " << output_path_bboxes.string() << "." << endl;
     }
 }
 
-void video_builder::build_video(const string &filename)
-{
-    vector<String> filenames;
-
-    fs::path output_directory("output");
-    fs::path videos_directory("videos");
-    output_directory /= videos_directory;
-    fs::create_directories(output_directory);
-
-    clear_input_video_info();
-    build_output_frames(filename, output_frames);
-
-    fs::path output_path;
-    output_path /= output_directory;
-    output_path /= fs::path(filename).filename();
-
-    cout << "Creating " << output_path.string() << "." << endl;
-    build_video_from_output_frames(output_frames, output_path.string());
-}
-
-void video_builder::build_output_frames(const string &filename, vector<Mat> &output_frames)
+void video_builder::build_output_frames(const string &filename, vector<Mat> &output_frames, vector<Mat> &bboxes_output_frames)
 {
     VideoCapture input_video(filename);
     if (!input_video.isOpened())
@@ -103,9 +87,13 @@ void video_builder::build_output_frames(const string &filename, vector<Mat> &out
         multi_tracker->add(legacy::TrackerCSRT::create(), first_frame, rescale_bounding_box(bbox, BOUNDING_BOX_RESCALE, MAX_BOUNDING_BOX_SIZE));
     }
 
+    bounding_boxes_drawer bboxes_drawer(pl_field_loc.get_localization(), balls_loc.get_localization(), multi_tracker->getObjects());
+    Mat bboxes_output_frame;
+    bboxes_drawer.draw(first_frame, bboxes_output_frame, multi_tracker->getObjects());
+    bboxes_output_frames.push_back(bboxes_output_frame);
+
     minimap mini(pl_field_loc.get_localization(), balls_loc.get_localization(), multi_tracker->getObjects());
     Mat pool_table_map;
-
     Mat output_frame;
     mini.draw_initial_minimap(pool_table_map);
     build_output_frame(first_frame, pool_table_map, output_frame);
@@ -117,18 +105,18 @@ void video_builder::build_output_frames(const string &filename, vector<Mat> &out
         multi_tracker->update(frame);
         mini.update(multi_tracker->getObjects());
         mini.draw_minimap(pool_table_map);
+        bboxes_drawer.draw(frame, bboxes_output_frame, multi_tracker->getObjects());
 
         build_output_frame(frame, pool_table_map, output_frame);
         output_frames.push_back(output_frame);
+        bboxes_output_frames.push_back(bboxes_output_frame);
     }
 
     // Write last minimap frame to disk
     string filename_no_path = fs::path(filename).filename();
-    fs::path output_directory("output");
-    fs::path videos_directory("videos");
     const string PNG_EXTENSION = ".png";
-    fs::path filename_png("last_frame_minimap_" + filename_no_path.substr(0, filename_no_path.find_first_of(".")) + PNG_EXTENSION);
-    fs::path last_frame_path = output_directory / videos_directory / filename_png;
+    fs::path filename_png(filename_no_path.substr(0, filename_no_path.find_first_of(".")) + PNG_EXTENSION);
+    fs::path last_frame_path = output_directory / last_frames_minimap_directory / filename_png;
     imwrite(last_frame_path.string(), pool_table_map);
 }
 
@@ -173,6 +161,7 @@ void video_builder::build_video_from_output_frames(const vector<Mat> &output_fra
 void video_builder::clear_input_video_info()
 {
     output_frames.clear();
+    bboxes_output_frames.clear();
     input_video_fps = -1;
     input_video_size = Size(-1, -1);
 }
